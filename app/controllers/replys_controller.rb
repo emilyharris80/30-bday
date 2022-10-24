e722bd1ba806e7669fd33323f1e0a0397e9b1e6c
Emily Harris <emily.harris@simplybusiness.co.uk>
2022-10-24 14:01:54 +0100

    Add database to track attendees.

--- app/controllers/replys_controller.rb
+++ app/controllers/replys_controller.rb
@@ -1,8 +1,10 @@
 class ReplysController
 
     def new
-        @reply = Reply.new(params[:fname], params[:reply])
-        
+        @reply = Reply.new(param[:fname], param[:reply])
+        CSV.open("/Users/emily.harris/personal/30-bday/emily_is_30/db/csv/attendees.csv", "ab") do |csv|
+            csv << [param[:fname], param[:reply]]
+        end
     end
 
     def index
